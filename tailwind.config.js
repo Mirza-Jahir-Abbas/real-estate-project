226435f774e4115fafeda556e2be0bab22110302
Rohit <rohitmirza8972@gmail.com>
2024-05-05 15:19:47 +0530

    27/04/2024 desktop 90% done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ export default {
         sm:"0.75rem",
         base:"0.875rem",
         md:"1rem",
-        lg:"1.2575rem",
+        lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
-        "3xl":"2.25rem"
+        "3xl":"2.25rem",
+        "4xl":"3rem"
       },
       backgroundImage: {
         'team-bg-image': "url('/public/teams/team.png')",
